205268caa7caa7e7f6f802cbe5600480b8ab9137
Harsh Kumar <harshkumar9.in@gmail.com>
2020-05-31 01:49:58 +0530

    board created

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const boxclick=document.getElementById('board')
+
+const game = {
+    State: 0,
+    Turn: 'X',
+    move: 0,
+}
+
 function boxClicked(r,c)
 {
+    game.move++;
     console.log('box clicked=' ,r,c);
     nextTurn()
+    let click = boxclick.children[0].children[r-1].children[c-1]
+    click.textContent=game.Turn
+    // console.log(game.move)
+    if(game.move===9)
+        alert("Game Over")
+
 }
 
 
-const game={
-    State : 0,
-    Turn : 'X'
-}
+
 
 const playerSpan=document.getElementById('player')
 
