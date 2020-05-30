2897b276fde798bf78570eab82bd2bca3ebd9b87
Harsh Kumar <harshkumar9.in@gmail.com>
2020-05-31 01:00:33 +0530

    basic setup

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function boxClicked(r,c)
 {
     console.log('box clicked=' ,r,c);
+    nextTurn()
+}
+
+
+const game={
+    State : 0,
+    Turn : 'X'
+}
+
+const playerSpan=document.getElementById('player')
+
+function nextTurn(){
+
+    if(game.Turn === 'X')
+        game.Turn='O'
+    else game.Turn='X'
+
+
+    playerSpan.textContent=game.Turn
+
 }
