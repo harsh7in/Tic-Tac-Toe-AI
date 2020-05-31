b4d16ec5fcd2219926986d3f0b3c7db101e6ad7c
Harsh Kumar <harshkumar9.in@gmail.com>
2020-05-31 23:53:34 +0530

    conditions added

--- script.js
+++ script.js
@@ -6,15 +6,29 @@ const game = {
     move: 0,
 }
 
+function isRowSame(r) {
+    let row = Array.from(boxclick.children[0].children[r - 1].children)
+    let winCondition = game.Turn + game.Turn + game.Turn
+    if (row.map(item => item.textContent).join('') === winCondition){
+        alert(" Game Over   ||   Winner =>  " + game.Turn)
+    }
+
+}
+
 function boxClicked(r,c)
 {
     game.move++;
     console.log('box clicked=' ,r,c);
-    nextTurn()
+    
     let click = boxclick.children[0].children[r-1].children[c-1]
     click.textContent=game.Turn
     // console.log(game.move)
-    if(game.move===9)
+    
+    isRowSame(r)
+     
+    nextTurn()
+
+    if (game.move === 9)
         alert("Game Over")
 
 }
